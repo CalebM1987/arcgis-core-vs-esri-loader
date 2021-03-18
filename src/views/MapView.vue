aa0a62bdc036b89e5711a2d5e4dba68b28a8c5b7
Caleb Mackey <caleb.mackey@gmail.com>
2021-03-18 13:52:02 -0500

    feat(MapView.vue): @arcgis/core version

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 /// <reference types="@types/arcgis-js-api" />
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, nextTick } from 'vue'
 import Map from '@arcgis/core/Map'
 import MapView from '@arcgis/core/views/MapView'
 import esriConfig from '@arcgis/core/config'
@@ -31,25 +31,19 @@ export default defineComponent({
       zoom: 12
     })
 
-    onMounted(()=> {
-      const view = new MapView({
-        map,
-        container: 'viewDiv',
-        center: [-94, 45],
-        zoom: 12
-      })
-
-      view.when(()=> {
-        console.log('view loaded')
-      })
-      
-    })
-
     return {
       map,
       view
     }
+  },
 
+  created(){
+    console.log('map and view: ', this.map, this.view)
+    nextTick(()=> this.view.container = this.$refs.viewDiv as HTMLDivElement)
+    this.view.when(()=> {
+      console.log('view loaded')
+    })
+    
   }
 
 })

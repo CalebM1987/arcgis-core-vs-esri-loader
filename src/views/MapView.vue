0432a238839a9fd2b063c5115b5b735f12195b48
Caleb Mackey <caleb.mackey@gmail.com>
2021-03-18 23:41:50 -0500

    feat(MapView): Mapbox gl example

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,16 +1,16 @@
 <template>
-  <!-- <div class="map-wrapper">
-    <div id="map" ref="mapDiv"></div>
-  </div> -->
-  <mapbox-map :accessToken="accessToken" :mapStyle="mapStyle"/>
+  <mapbox-map 
+    :accessToken="accessToken" 
+    mapStyle="streets-v11"
+    :center="[-95, 45]"
+    :zoom="12"
+    height="800px"
+  />
 </template>
 
 <script lang="ts">
-// import { Map } from '@types/mapbox-gl'
 import { defineComponent, ref } from 'vue'
 import { MapboxMap } from 'vue-mapbox-ts'
-// import { Map } from "mapbox-gl"
-// import { MglMap } from "vue-mapbox" // eslint-disable-line
 
 
 export default defineComponent({
@@ -21,34 +21,14 @@ export default defineComponent({
     
     // set api key (use .env.local file)
     const accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
-
-    const mapStyle = ref('streets-v11')
-
-
-
-    // set map 
-
-
+    
     return {
       accessToken,
-      mapStyle,
-      // map,
-      // map,
-      // view
     }
   },
 
   created(){
-    console.log('mapbox map created', this.$refs.mapDiv, document.getElementById('map'))
-    // const map: Map = new Map({
-    //   container: 'map', //this.$refs.mapDiv,
-    //   style: this.mapStyle,
-    //   center: [-94, 45],
-    //   zoom: 12
-    // })
-
-    // nextTick(()=> this.map.container = this.$refs.mapDiv)
-    
+    console.log('mapbox map created')
   }
 
 })
